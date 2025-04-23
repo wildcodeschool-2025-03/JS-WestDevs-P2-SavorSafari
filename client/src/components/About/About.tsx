6193ef013a4a4b21bc81ea9663fb76ecdb2e9e8d
anne durand <annedurand0709@gmail.com>
2025-04-23 17:50:16 +0200

    creat page about

--- client/src/components/About/About.tsx
+++ client/src/components/About/About.tsx
@@ -1,5 +1,27 @@
+import "./About.css";
+
 const About = () => {
-  return <p>About</p>;
+  return (
+    <>
+      <div className="avatar woman">
+        <img src="/src/assets/img/avatar_projet_2.webp" alt="avatar woman" />
+      </div>
+
+      <img src="/src/assets/img/SavorLogo.png" alt="logo" />
+
+      <article className="text">
+        <h1>About us:</h1>
+        <p>
+          At SavorSafari, we believe that cooking is much more than just a way
+          to nourish ourselves; it's a sensory adventure that allows us to
+          explore new cultures and create unforgettable memories. That’s why
+          we’ve created a unique interactive platform that lets you travel the
+          world right from your kitchen.
+        </p>
+        <h2>We wish you a wonderful journey!</h2>
+      </article>
+    </>
+  );
 };
 
 export default About;
