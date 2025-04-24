39b425915d017a566eba8ad652b51c1600bf7a31
anne durand <annedurand0709@gmail.com>
2025-04-24 16:09:46 +0200

    version1 about

--- client/src/components/About/About.tsx
+++ client/src/components/About/About.tsx
@@ -3,23 +3,31 @@ import "./About.css";
 const About = () => {
   return (
     <>
-      <div className="avatar woman">
-        <img src="/src/assets/img/avatar_projet_2.webp" alt="avatar woman" />
-      </div>
-
-      <img src="/src/assets/img/SavorLogo.png" alt="logo" />
-
-      <article className="text">
-        <h1>About us:</h1>
-        <p>
-          At SavorSafari, we believe that cooking is much more than just a way
-          to nourish ourselves; it's a sensory adventure that allows us to
-          explore new cultures and create unforgettable memories. That’s why
-          we’ve created a unique interactive platform that lets you travel the
-          world right from your kitchen.
-        </p>
-        <h2>We wish you a wonderful journey!</h2>
-      </article>
+      <main className="about">
+        <img className="logo" src="src/assets/img/SavorLogo.png" alt="logo" />
+        <img
+          className="avatar"
+          src="src/assets/img/avatar_projet_2 (1) - rogner.png"
+          alt="avatar"
+        />
+        <article>
+          <h1>
+            <img
+              src="src/assets/img/illustration spatules.webp"
+              alt="ustensiles"
+            />
+            About us:
+          </h1>
+          <p>
+            At SavorSafari, we believe that cooking is much more than just a way
+            to nourish ourselves; it's a sensory adventure that allows us to
+            explore new cultures and create unforgettable memories. That’s why
+            we’ve created a unique interactive platform that lets you travel the
+            world right from your kitchen.
+          </p>
+          <h2>We wish you a wonderful journey!</h2>
+        </article>
+      </main>
     </>
   );
 };
