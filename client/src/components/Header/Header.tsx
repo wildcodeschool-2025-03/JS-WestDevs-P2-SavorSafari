906f34d71bbdbf6cf6d93b4597cd62ca37d3c995
anne durand <annedurand0709@gmail.com>
2025-04-30 09:40:39 +0200

    biome bug

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -17,16 +17,13 @@ const Header = () => {
       </header>
 
       <Menu right={true}>
-        <Link id="home" className="menu-item" to="/">
-          Home
-        </Link>
-        <Link id="map" className="menu-item" to="/world-map">
-          Map
-        </Link>
-        <Link id="about" className="menu-item" to="/about">
-          About
-        </Link>
-        <Link id="register" className="menu-item" to="/register"></Link>
+        <Link id="home" className="menu-item" to="/" />
+        Home
+        <Link id="map" className="menu-item" to="/world-map" />
+        Map
+        <Link id="about" className="menu-item" to="/about" />
+        About
+        <Link id="register" className="menu-item" to="/register" />
       </Menu>
     </>
   );
