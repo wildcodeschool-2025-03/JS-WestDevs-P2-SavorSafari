80d39c2a09b924a68644c5493cd9ae5430cbbf26
Romain-dev85 <r.auvinet@gmail.com>
2025-05-02 12:35:12 +0200

    add form

--- client/src/components/Register/Register.tsx
+++ client/src/components/Register/Register.tsx
@@ -1,11 +1,44 @@
+import { useState } from "react";
 import "./Register.css";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event: React.FormEvent) => event.preventDefault();
+
   return (
     <main className="register-page">
       <img src="./public/img/SavorLogo.png" alt="logo savorsafari" />
       <h1>Log in</h1>
       <h2>It's a pleasure to see you again!</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              placeholder="E-mail or username"
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="password">
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+              placeholder="Password"
+            />
+          </label>
+        </div>
+        <button type="submit">Log in</button>
+        <p>or</p>
+        <button type="submit">Create an account</button>
+      </form>
     </main>
   );
 };
