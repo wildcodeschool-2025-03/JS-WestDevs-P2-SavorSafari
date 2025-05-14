25e59dd7e9d5ed9db600abbd7716f723930e9d3c
tomabadie <abadie.thomas@gmail.com>
2025-05-14 16:43:27 +0200

    Modified space issue to match linter check

--- client/src/components/WorldMap/WorldMap.tsx
+++ client/src/components/WorldMap/WorldMap.tsx
@@ -69,11 +69,11 @@ const WorldMap = () => {
   const [query, setQuery] = useState("");
 
   const filteredCountries =
-    query === "" ?
-      availableCountries :
-      availableCountries.filter((country) => {
-        return country.name.toLowerCase().includes(query.toLowerCase());
-      });
+    query === ""
+      ? availableCountries
+      : availableCountries.filter((country) => {
+          return country.name.toLowerCase().includes(query.toLowerCase());
+        });
 
   return (
     <section className="world-map-section">
@@ -117,7 +117,8 @@ const WorldMap = () => {
               aria-label="Assignee"
               displayValue={(country: CountryProps) => country?.name}
               onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-                setQuery(event.target.value)}
+                setQuery(event.target.value)
+              }
             />
             <ComboboxButton className="combobox-button">
               <svg
